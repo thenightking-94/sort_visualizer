46c5951bdbb96715ea25890678ef4f81b30f649b
Shubham Chatterjee <c.suvam@gmail.com>
2020-12-26 05:43:09 +0530

    some dynamics added

--- src/Comps/Dashboard.js
+++ src/Comps/Dashboard.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import '../CSS/dashboard.css';
+import { Card, Select, MenuItem } from '@material-ui/core';
 
 const Dashboard = () => {
     return (
         <div>
+            <Card className='header flexRow'>
+                Data Visualizer
+            </Card>
+            <br />
+            <div className='flexRow'>
+
+                <Select
+                    value={null}
+                    onChange={() => {
+                        console.log('we are changing')
+                    }}
+                    className='selecComp'
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+                &nbsp;&nbsp;
+                <Select
+                    value={null}
+                    onChange={() => {
+                        console.log('we are changing')
+                    }}
+                    className='selecComp'
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </div>
 
         </div>
     )
